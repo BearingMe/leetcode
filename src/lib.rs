9ad08fd7dca06a089e5b560aacf6904d1403f54e
BearingMe <gomes.bruno201@gmail.com>
2024-06-05 23:23:00 -0300

    chore: add initial project files and structure

---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+pub mod problems {
+    pub mod ex001;
+}
---
+++ src/problems/ex001.rs
@@ -0,0 +1,13 @@
+pub fn main() {
+    println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_main() {
+        main();
+    }
+}
