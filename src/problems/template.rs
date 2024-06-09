806d27d26f29b166550135bf67da89d070f2b85e
BearingMe <gomes.bruno201@gmail.com>
2024-06-08 22:58:14 -0300

    feat: add exercise template

---
+++ src/problems/template.rs
@@ -0,0 +1,15 @@
+#[allow(warnings)]
+#[cfg(test)]
+mod ex_test {
+    use super::*;
+
+    #[test]
+    fn case_1() {
+        let input = ();
+        let output = ();
+
+        let result = ();
+
+        assert_eq!(output, result);
+    }
+}
